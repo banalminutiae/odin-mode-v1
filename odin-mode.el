ace2c5fe432e282816833d4dcb7b2f7ed63f448e
Andrew Li <andrewli-1920@hotmail.com>
2022-03-07 18:55:37 +0800

    Mode successfully loads and lints strings.

--- odin-mode.el
+++ odin-mode.el
@@ -6,6 +6,18 @@
 
 ;; You can redistribute this program and/or modify it under the terms of the GNU General Public License version 2.
 
+(require 'rx)
+
+(defvar odin-mode-v1-hook-nil
+  "Hook for odin major mode.")
+(defcustom odin-mode-v1-tab-width 4
+  "Width of tabs for 'odin-mode'.")
+(defcustom arm-comment-char "//"
+  "Character to denote inline comments.")
+
+;;;#autoload
+(add-to-list 'auto-mode-alist '("\\.odin\\'" . odin-mode-v1))
+
 (defconst odin-mode-v1-keywords
   '("package" "import" "proc"
     "for" "do" "in" 
@@ -52,39 +64,51 @@
     "quaternion64" "quaternion128" "quaternion256"
 
     "rune" "string" "cstring"
-    "rawptr" "typeid" "any"))
+    "rawptr" "typeid" "any" "byte"))
 
 (defvar odin-mode-v1-tab-width 4 "Width of a tab for Odin mode")
 
-(setq odin-mode-v1-font-lock-keywords
-      (let *(
-            ;; keywords and regex
-            )
-        '(
-          ;; mapping to faces
-          )))
+(defconst odin-mode-v1-defaults
+  '((
+     ;; text between double quotes
+     ("\"\\.\\*\\?" . font-lock-string-face)
+     (":\\|,\\|;\\|{\\|}\\|=>\\|@\\|#\\|=\\|&\\|+\\|%\\|<\\|>\\|'\\|^\\|" . font-lock-keyword-face) ;; ew, replace with syntax table entries
+     ( ,(regexp-opt odin-mode-v1-keywords 'words) . font-lock-builtin-face)
+     ( ,(regexp-opt odin-mode-v1-types 'words) . font-lock-type-face)
+     ;; somehow mangle regex to detect variables for font-lock-variable-name-face
+     )))
+
+
+;; (defconst odin-mode-syntax-table
+;;   (let ((st (make-syntax-table)))
+;;     (modify-syntax-entry ?\" "\"" st)
+;;     (modify-syntax-entry ?\\ "\\" st)
+;;     (modify-syntax-entry ?#   "_" st)
+;;     ;; Modify some syntax entries to allow ne sted block comments
+;;     (modify-syntax-entry ?/ ". 124b" st)
+;;     (modify-syntax-entry ?* ". 23n" st)
+;;     (modify-syntax-entry ?\n "> b" st)
+;;     (modify-syntax-entry ?\^m "> b" st)
+;;     st))
 
 
 ;;;###autoload
-(define-derived-mode odin-mode-v1 fundamental-mode "odin-mode-v1" ;; derive from another mode, add its keywords
+(define-derived-mode odin-mode-v1 prog-mode "odin-mode-v1"
   "Major mode for editing the Odin language..."
-  (setq font-lock-defaults odin-mode-v1-keywords)
+  (setq font-lock-defaults '(odin-mode-v1-defaults))
 
+  ;; this hopefully invokes emac's default tab behaviour of auto-formatting
+  ;; over uniformly inserting spaces
   (when odin-mode-v1-tab-width
     (setq tab-width odin-mode-v1-tab-width))
-  
-  (setq-comment-start "//")
-  (setq-comment-start "/*")
-  (setq-comment-end "*/")
 
-  (modify-syntax-entry ?// "< b" odin-mode-v1-sytax-table)
-  (modify-syntax-entry ?\n "> b" odin-mode-v1-syntax-table)
-
+  (setq-local comment-start-skip "\\(//+\\|/\\*+\\)\\s *")
+  (setq-local tab-always-indent nil)
+  (setq-local comment-start "//")
+  (setq-local comment-start "/*")
+  (setq-local comment-end "*/")
 )
 
-;;;#autoload
-(add-to-list 'auto-mode-alist '("\\.odin\\'" . odin-mode-v1))
-  
 (provide 'odin-mode-v1)
 
 ;;; odin-mode.el ends here
